25c9d99f70ab8d29d34cbff15d4ba2b347cfbdb1
Žáček Radek <Zacek.Ra.2022@skola.ssps.cz>
2022-10-11 11:32:06 +0200

    init

---
+++ cykly/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace cykly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocet = int.Parse(Console.ReadLine());
+            int min = Int32.MaxValue;
+            int max = -1;
+            int soucet = 0;
+            for (int i = 0; i < pocet; i++)
+            {
+                int tmp = int.Parse(Console.ReadLine());
+                if (tmp<min)
+                {
+                    min = tmp;
+                }
+                if (tmp>max)
+                {
+                    max = tmp;
+                }
+                soucet = soucet + tmp;
+            }
+            int prumer = soucet / pocet;
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+            Console.WriteLine(prumer);
+        }
+    }
+}
